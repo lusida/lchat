f0b860806bb70f8eb36082caa6556c9ff7273169
SIDA-LU <43487524+lusida@users.noreply.github.com>
2021-12-12 18:15:05 +0800

    demo

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,8 +6,10 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import JwChat from 'jwchat'
 
 Vue.use(ElementUI)
+Vue.use(JwChat)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
